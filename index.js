4badb19878b7095c35beeb47e9d3d14878ae2172
TheRealGeneParmesan <Geneparmigiano@gmail.com>
2023-02-27 15:25:19 -0800

    updated gitignore file and built the createSVG function, which creates an SVG image

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const imagePrompt = [{
 function init() {
     inquirer.prompt(imagePrompt).then((answers) => {
         const { character, color, shape } = answers;
-        const svgFile = svgTemplate(character, color, shape);
+        const svgFile = createSVG(character, color, shape);
 
 
         fs.writeFile('./examples/logo.svg', svgFile, (err) => {
@@ -36,8 +36,23 @@ function init() {
                 console.log("error")
             else (console.log(`Generated logo.svg`))
         });
-    }
-    )
+
+        // I was able to get most of the formatting for the SVG formatting below from the mozilla tutorial. 
+
+        // The createSVG function below creates an SVG image taking the shape, color and 3 letter text that the user inputs and returns a string containing the elements required to draw the shape as well as the viewbox attribute, which scales the SVG element to have a width of 300 and a height of 200 (Mozilla viewBox documentation).
+
+        function createSVG(characters, color, shape) {
+            let svgString = '';
+            if (shape === 'circle') {
+                svgString = svgString.concat(`<circle cx="150" cy="100" r="70" fill="${color}"/> <text x ="50%" y = "50%" text-anchor = "middle" fill = "white"> ${characters}</text>`);
+            } else if (shape === 'triangle') {
+                svgString = svgString.concat(`<polygon points="150, 18 244, 182 56, 182" fill ="${color}"/> <text x ="150" y="110" text-anchor="middle" fill = "white"> ${characters}</text>`);
+            } else if (shape === 'square') {
+                svgString = svgString.concat(`<rect x="90" y="40" width="120" height="120" fill="${color}"/> <text x = "50%" y = "50%" text-anchor="middle" fill= "white"> ${characters}</text>`);
+            }
+            return `<svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg">${svgString}</svg>`;
+        }
+    })
 }
 
 init()
